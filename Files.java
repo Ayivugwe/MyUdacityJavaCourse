91ecc76d5e9a3f78415add93fd96f85fd9c59adf
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2019-09-24 10:53:08 +0200

    Update Files.java

--- Files.java
+++ Files.java
@@ -1,18 +1,12 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class Files() throws Exception{
+public class Files(){
 public static void main(String [] args){
 
-File file = new File("expenses.txt");
-Scanner fileScanner = new Scanner(file);
-int words=0;
-while(input.hasNextLine()){
-String line = input.nextLine();
-  words+=line.split(" ").length;
-
+System.out.println("Je test les fichiers en java");
+ // File file = new File("exemple1.txt");
+ // Scanner fileScanner = new Scanner(file);
+  
+  }
 }
-  System.out.println(words)
-}
-}
-
