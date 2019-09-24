de493cc11386c5e72faccb3bbefb7e456104c7cd
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2019-09-24 09:39:18 +0200

    Create Files.java

---
+++ Files.java
@@ -0,0 +1,17 @@
+public class Files(){
+public static void main(String [] args){
+
+File file = new File("expenses.txt");
+Scanner fileScanner = new Scanner(file);
+
+
+
+/*
+while(input.hasNextLine()){
+String line = input.gasNextLine();
+System.out.println(line);
+
+}*/
+}
+}
+
