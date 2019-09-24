45b68082e4b83e5702bd041feaaeb21535bebd40
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2019-09-24 09:44:15 +0200

    Create ReadFile.java

---
+++ ReadFile.java
@@ -0,0 +1,12 @@
+public class ReadFile(){
+  public static void main(String [] args){
+  File file= new File();
+  Scanner fileScanner = new Scanner(file);
+  
+  while (input.hasNextLine()) {
+   String line = input.nextLine();
+   // Use that line to do any calculations, processing, etc ..
+}
+  
+  }
+}
